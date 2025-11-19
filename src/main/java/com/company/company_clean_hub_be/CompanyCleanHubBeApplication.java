501cc8c0959c754a9735d271f88ad120eff8947b
daile269 <daile2692003@gmail.com>
2025-11-19 11:20:23 +0700

    init project

---
+++ src/main/java/com/company/company_clean_hub_be/CompanyCleanHubBeApplication.java
@@ -0,0 +1,13 @@
+package com.company.company_clean_hub_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyCleanHubBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyCleanHubBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/company_clean_hub_be/CompanyCleanHubBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.company_clean_hub_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyCleanHubBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
