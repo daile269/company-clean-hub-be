dfa0955be62a7c45431a42ec8a208e955c928dc6
daile269 <daile2692003@gmail.com>
2026-01-15 08:38:14 +0700

    update plan day

--- src/main/java/com/company/company_clean_hub_be/entity/ContractType.java
+++ src/main/java/com/company/company_clean_hub_be/entity/ContractType.java
@@ -1,7 +1,7 @@
 package com.company.company_clean_hub_be.entity;
 
 public enum ContractType {
-
-
-
+    ONE_TIME,           // Hợp đồng 1 lần (trọn gói)
+    MONTHLY_FIXED,      // Hợp đồng hàng tháng cố định (chi phí cố định theo tháng)
+    MONTHLY_ACTUAL      // Hợp đồng hàng tháng theo ngày thực tế (chi phí theo số ngày làm thực tế)
 }
